c4097519764b7d11a9689c4b14e4a2a1e9ae4afa
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-04-12 15:48:11 -0400

    Add RouteSwitch component
    Setup static version of Header component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <h1>Store</h1>
+      <nav>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </nav>
+    </React.Fragment>
+  );
+};
+
+export default Header;
