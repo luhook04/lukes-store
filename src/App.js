0c9a72730c0bf38e100941fed8100d2297707824
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-04-13 17:33:58 -0400

    Add another state and fetchStore function
    Make a store state that holds the result of fetchStore function. This
    will give the user a selection of products to choose from.
    
    Pass store in product component's state

--- src/App.js
+++ src/App.js
@@ -8,32 +8,31 @@ import ShoppingCart from "./components/ShoppingCart";
 import Header from "./components/Header";
 
 const App = () => {
+  const [ cost, setCost ] = useState(0);
+  const [ cart, setCart ] = useState([]);
+  const [ store, setStore ] = useState([]);
+
   useEffect(() => {
     fetchStore();
   }, []);
 
-  //const [ cost, setCost ] = useState(0);
-  //const [ cart, setCart ] = useState([]);
-
   const fetchStore = async () => {
-    const response = await fetch(
-      "https://fakestoreapi.com/products?limit=5"
-    );
-    console.dir(response.json());
+    const url = "https://fakestoreapi.com/products";
+    const response = await fetch(url);
+    const data = await response.json();
+    setStore(data);
   };
 
-  return <div />;
-
-  // return (
-  //   <Router>
-  //     <Header />
-  //     <Routes>
-  //       <Route path="/" element={Home} />
-  //       <Route path="/shop" element={Products} />
-  //       <Route path="/cart" element={ShoppingCart} />
-  //     </Routes>
-  //   </Router>
-  // );
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Products store={store} />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
