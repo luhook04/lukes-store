0255f7539369fc6b9be32d74c43709dc64ff797d
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-04-13 18:30:25 -0400

    Add button to ProductCard

--- src/components/store/ProductCard.js
+++ src/components/store/ProductCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-const ProductCard = () => {};
+const ProductCard = ({ key, name, price, image }) => {
+  return (
+    <div key={key}>
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <h3>{price}</h3>
+      <button>Add to cart</button>
+    </div>
+  );
+};
 
 export default ProductCard;
