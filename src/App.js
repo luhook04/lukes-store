cb2fa41a03cca86ba33054fd2a080ad85a6977dd
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-04-13 13:46:55 -0400

    Changed to React 18 syntax
    In index.js I used the new createRoot syntax instead of the old
    ReactDom.render method.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import "./App.css";
 import React from "react";
-import { useState, useEFfect } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Products from "./components/store/Products";
 import ShoppingCart from "./components/ShoppingCart";
+import Header from "./components/Header";
 
 const App = () => {
-  const [ cost, setCost ] = useState(0);
-  const [ boughtItems, setBoughtItems ] = useState([]);
+  useEffect(() => {
+    fetchStore();
+  }, []);
 
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={Home} />
-        <Route path="/shop" element={Products} />
-        <Route path="/cart" element={ShoppingCart} />
-      </Routes>
-    </Router>
-  );
+  //const [ cost, setCost ] = useState(0);
+  //const [ cart, setCart ] = useState([]);
+
+  const fetchStore = async () => {
+    const response = await fetch(
+      "https://fakestoreapi.com/products?limit=5"
+    );
+    console.dir(response.json());
+  };
+
+  return <div />;
+
+  // return (
+  //   <Router>
+  //     <Header />
+  //     <Routes>
+  //       <Route path="/" element={Home} />
+  //       <Route path="/shop" element={Products} />
+  //       <Route path="/cart" element={ShoppingCart} />
+  //     </Routes>
+  //   </Router>
+  // );
 };
 
 export default App;
