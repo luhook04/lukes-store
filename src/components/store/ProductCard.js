285a63b27893906989cbd04f77e2ff9ef98ffe47
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-04-13 21:07:59 -0400

    Fix error in product card
    Fix error by deleting div id of key

--- src/components/store/ProductCard.js
+++ src/components/store/ProductCard.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const ProductCard = ({ key, name, price, image }) => {
+const ProductCard = ({ name, price, image }) => {
   return (
-    <div key={key}>
+    <div>
       <img src={image} alt={name} />
       <h3>{name}</h3>
       <h3>${price.toFixed(2)}</h3>
