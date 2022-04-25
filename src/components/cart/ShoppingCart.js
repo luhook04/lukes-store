2ac26d1add17cd44d9b3f9d9ad550358fe567274
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-04-25 12:01:52 -0400

    Show total in ShoppingCart component
    Add a variable (total) to keep track of the mapped over items price.
    Then display the toal variable.

--- src/components/cart/ShoppingCart.js
+++ src/components/cart/ShoppingCart.js
@@ -2,8 +2,11 @@ import React from "react";
 import CartItem from "./CartItem";
 
 const ShoppingCart = ({ cart }) => {
+  let total = 0;
+
   if (cart.length !== 0) {
     const items = cart.map((item, index) => {
+      total += item.price;
       return (
         <CartItem
           key={index}
@@ -14,7 +17,12 @@ const ShoppingCart = ({ cart }) => {
       );
     });
 
-    return <div className="cart-grid">{items}</div>;
+    return (
+      <div>
+        <h3>total: ${total.toFixed(2)}</h3>
+        <div className="cart-grid">{items}</div>
+      </div>
+    );
   }
   else {
     return <h2>Your cart is currently empty</h2>;
