6e95ad0f6e3de8c93e7ea8c49442b151272652bd
Luke Hook <lukehook04@gmail.com>
2023-01-18 15:58:20 -0500

    Change the basename for the hash router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <Router basename="/lukes-store">
+  <Router basename="/">
     <App />
   </Router>
 );
