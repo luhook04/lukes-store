9f93d8d37948b3eb55cf92f9d078e1454a6a4f54
Luke Hook <lukehook04@gmail.com>
2023-01-18 15:54:11 -0500

    Switch to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
